8fcddcd6a2b3853291eda687cb5caf4498498fba
wade-web <freedevweb221@gmail.com>
2025-10-06 18:21:35 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,10 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
-// // https://vitejs.dev/config/
+// // https://vite.dev/config/
 // export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       '/api': {
-//         target: 'https://tp-react-snowy.vercel.app',
-//         changeOrigin: true
-//       }
-//     }
-//   }
+//   plugins: [react()],
 // })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -33,13 +15,11 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      // CORRECTION : Pointez vers votre backend Render, pas vers Vercel
       '/api': {
-        target: 'https://backend-tp-km23.onrender.com', // Votre backend Render
-        changeOrigin: true,
-        secure: false, // Important pour Render
-        rewrite: (path) => path
+        target: 'https://backend-tp-km23.onrender.com',
+        changeOrigin: true
       }
     }
   }
 })
+
