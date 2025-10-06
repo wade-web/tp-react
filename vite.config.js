e58570d9b823d7fa0e7e8c162c7632555a2f367a
wade-web <freedevweb221@gmail.com>
2025-10-06 18:34:31 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://backend-tp-km23.onrender.com',
         changeOrigin: true
       }
     }
