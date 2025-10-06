8a5fa2429801414c7b79b1f8ad4b40dfcbb3b4a9
wade-web <freedevweb221@gmail.com>
2025-10-06 17:36:44 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://tp-react-snowy.vercel.app',
         changeOrigin: true
       }
     }
